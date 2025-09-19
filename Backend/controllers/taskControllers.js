237be46eba9f0f90f356cce5cea60cf827952011
gaurav-xt <gauravtambare7@gmail.com>
2025-09-20 00:55:11 +0530

    controllers

--- Backend/controllers/taskControllers.js
+++ Backend/controllers/taskControllers.js
@@ -2,6 +2,8 @@ import { Task } from '../models/Task.js'
 
 export const getTasks = async (req,res) =>{
     try {
+        const task = await Task.find() // find the task in database
+        res.json(task) // send those task back to frontend
         
     } catch (error) {
         console.log(error)
@@ -11,7 +13,9 @@ export const getTasks = async (req,res) =>{
 
 export const createTasks = async (req,res)=>{
     try {
-        
+        const task = new Task(req.body)
+        await task.save()
+        res.status(201).json(task)
     } catch (error) {
         console.log(error)
         
@@ -21,7 +25,11 @@ export const createTasks = async (req,res)=>{
 
 export const updateTasks = async (req,res)=>{
     try {
-        
+       const task = await Task.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        {new:true}
+       ) 
     } catch (error) {
         console.log(error)
         
@@ -30,7 +38,8 @@ export const updateTasks = async (req,res)=>{
 
 export const deleteTasks = async (req,res) =>{
     try {
-        
+        await Task.findByIdAndDelete(req.params.id)
+        res.json({message: "Task deleted"})
     } catch (error) {
         console.log(error)
         
@@ -40,7 +49,10 @@ export const deleteTasks = async (req,res) =>{
 
 export const toggleComplete = async (req , res) =>{
     try {
-        
+       const task = await Task.findById(req.params.id) 
+       task.completed = !task.completed
+       task.save()
+       res.json(task)
     } catch (error) {
         console.log(error)
         
